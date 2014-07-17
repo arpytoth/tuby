2f55991ac91ccdb7f3969336bbaebc282949bbc9
Toth Arpad <arpytoth@yahoo.com>
2014-07-17 16:27:37 -0400

    Added while to tokenizer

--- tuby/tokenizer.c
+++ tuby/tokenizer.c
@@ -1,29 +1,12 @@
 /* 
  * Copyright (C) 2014 Toth Arpad 
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License along
- *   with this program; if not, write to the Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *    
- *   Toth Arpad (arpytoth@yahoo.com)
  */
-
-#include "tokenizer.h"
-#include "utils.h"
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "utils.h"
+#include "tokenizer.h"
 
 /* Global source file used to basically to store the entire source code. */
 struct SourceFile g_source;
@@ -221,8 +204,16 @@ int read_identifier()
     size = end - start + 2;
     strncpy(g_token.repr, g_source.buffer + start, size - 1);
     g_token.repr[size - 1] = '\0';
-    
-    if (strcmp(g_token.repr, "if") == 0)
+   
+    if (strcmp(g_token.repr, "while") == 0)
+    {
+        g_token.type = ttWhile;
+    }
+    else if (strcmp(g_token.repr, "do") == 0)
+    {
+        g_token.type = ttDo;
+    }
+    else if (strcmp(g_token.repr, "if") == 0)
     {
         g_token.type = ttIF;
     }
